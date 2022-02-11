fa3e306e1dece083d14504c31f9b7ebfa2e8482e
gmojicar <60418323+gmojicar@users.noreply.github.com>
2022-02-11 18:05:29 -0500

    Add files via upload

---
+++ build/software/bios/main.d
@@ -0,0 +1,139 @@
+main.o: /opt/Litex/litex/litex/soc/software/bios/main.c \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/tinystdio/stdio.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/inttypes.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/newlib.h \
+ /home/gmojicar/Escritorio/Digital_II/Proyecto/Proyecto_Grupo_4/wp04-2021-2-gr04-main/SoC_project/build/nexys4ddr/software/include/../libc/picolibc.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/config.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/ieeefp.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/features.h \
+ /opt/Litex/riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-linux-ubuntu14/lib/gcc/riscv64-unknown-elf/8.3.0/include/float.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_intsup.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/_ansi.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdint.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_default_types.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_stdint.h \
+ /opt/Litex/riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-linux-ubuntu14/lib/gcc/riscv64-unknown-elf/8.3.0/include/stddef.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_locale.h \
+ /opt/Litex/riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-linux-ubuntu14/lib/gcc/riscv64-unknown-elf/8.3.0/include/stdarg.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_types.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_types.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdlib.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/cdefs.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/stdlib.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/alloca.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/string.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/strings.h \
+ /opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/string.h \
+ /opt/Litex/litex/litex/soc/cores/cpu/picorv32/system.h \
+ /opt/Litex/litex/litex/soc/cores/cpu/picorv32/irq.h \
+ /home/gmojicar/Escritorio/Digital_II/Proyecto/Proyecto_Grupo_4/wp04-2021-2-gr04-main/SoC_project/build/nexys4ddr/software/include/generated/csr.h \
+ /home/gmojicar/Escritorio/Digital_II/Proyecto/Proyecto_Grupo_4/wp04-2021-2-gr04-main/SoC_project/build/nexys4ddr/software/include/generated/soc.h \
+ /opt/Litex/litex/litex/soc/software/include/hw/common.h \
+ /opt/Litex/litex/litex/soc/software/bios/boot.h \
+ /opt/Litex/litex/litex/soc/software/bios/readline.h \
+ /opt/Litex/litex/litex/soc/software/bios/helpers.h \
+ /opt/Litex/litex/litex/soc/software/bios/command.h \
+ /home/gmojicar/Escritorio/Digital_II/Proyecto/Proyecto_Grupo_4/wp04-2021-2-gr04-main/SoC_project/build/nexys4ddr/software/include/generated/mem.h \
+ /home/gmojicar/Escritorio/Digital_II/Proyecto/Proyecto_Grupo_4/wp04-2021-2-gr04-main/SoC_project/build/nexys4ddr/software/include/generated/git.h \
+ /opt/Litex/litex/litex/soc/software/libbase/console.h \
+ /opt/Litex/litex/litex/soc/software/libbase/crc.h \
+ /opt/Litex/litex/litex/soc/software/libbase/spiflash.h \
+ /opt/Litex/litex/litex/soc/software/libbase/uart.h \
+ /opt/Litex/litex/litex/soc/software/liblitedram/sdram.h \
+ /opt/Litex/litex/litex/soc/software/libliteeth/udp.h \
+ /opt/Litex/litex/litex/soc/software/libliteeth/mdio.h \
+ /opt/Litex/litex/litex/soc/software/liblitespi/spiflash.h \
+ /opt/Litex/litex/litex/soc/software/liblitesdcard/sdcard.h \
+ /opt/Litex/litex/litex/soc/software/liblitesata/sata.h
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/tinystdio/stdio.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/inttypes.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/newlib.h:
+
+/home/gmojicar/Escritorio/Digital_II/Proyecto/Proyecto_Grupo_4/wp04-2021-2-gr04-main/SoC_project/build/nexys4ddr/software/include/../libc/picolibc.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/config.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/ieeefp.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/features.h:
+
+/opt/Litex/riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-linux-ubuntu14/lib/gcc/riscv64-unknown-elf/8.3.0/include/float.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_intsup.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/_ansi.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdint.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_default_types.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_stdint.h:
+
+/opt/Litex/riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-linux-ubuntu14/lib/gcc/riscv64-unknown-elf/8.3.0/include/stddef.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_locale.h:
+
+/opt/Litex/riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-linux-ubuntu14/lib/gcc/riscv64-unknown-elf/8.3.0/include/stdarg.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/_types.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/_types.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/stdlib.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/cdefs.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/machine/stdlib.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/alloca.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/string.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/strings.h:
+
+/opt/Litex/pythondata-software-picolibc/pythondata_software_picolibc/data/newlib/libc/include/sys/string.h:
+
+/opt/Litex/litex/litex/soc/cores/cpu/picorv32/system.h:
+
+/opt/Litex/litex/litex/soc/cores/cpu/picorv32/irq.h:
+
+/home/gmojicar/Escritorio/Digital_II/Proyecto/Proyecto_Grupo_4/wp04-2021-2-gr04-main/SoC_project/build/nexys4ddr/software/include/generated/csr.h:
+
+/home/gmojicar/Escritorio/Digital_II/Proyecto/Proyecto_Grupo_4/wp04-2021-2-gr04-main/SoC_project/build/nexys4ddr/software/include/generated/soc.h:
+
+/opt/Litex/litex/litex/soc/software/include/hw/common.h:
+
+/opt/Litex/litex/litex/soc/software/bios/boot.h:
+
+/opt/Litex/litex/litex/soc/software/bios/readline.h:
+
+/opt/Litex/litex/litex/soc/software/bios/helpers.h:
+
+/opt/Litex/litex/litex/soc/software/bios/command.h:
+
+/home/gmojicar/Escritorio/Digital_II/Proyecto/Proyecto_Grupo_4/wp04-2021-2-gr04-main/SoC_project/build/nexys4ddr/software/include/generated/mem.h:
+
+/home/gmojicar/Escritorio/Digital_II/Proyecto/Proyecto_Grupo_4/wp04-2021-2-gr04-main/SoC_project/build/nexys4ddr/software/include/generated/git.h:
+
+/opt/Litex/litex/litex/soc/software/libbase/console.h:
+
+/opt/Litex/litex/litex/soc/software/libbase/crc.h:
+
+/opt/Litex/litex/litex/soc/software/libbase/spiflash.h:
+
+/opt/Litex/litex/litex/soc/software/libbase/uart.h:
+
+/opt/Litex/litex/litex/soc/software/liblitedram/sdram.h:
+
+/opt/Litex/litex/litex/soc/software/libliteeth/udp.h:
+
+/opt/Litex/litex/litex/soc/software/libliteeth/mdio.h:
+
+/opt/Litex/litex/litex/soc/software/liblitespi/spiflash.h:
+
+/opt/Litex/litex/litex/soc/software/liblitesdcard/sdcard.h:
+
+/opt/Litex/litex/litex/soc/software/liblitesata/sata.h:
